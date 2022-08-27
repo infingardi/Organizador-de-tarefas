5004251d89a853d49489a679d5de3d64a7ffa699
igor <infingardidev@gmail.com>
2022-08-27 01:50:25 -0300

    init: iniciando projeto java

---
+++ src/main/java/com/infingardi/organizadordetarefas/OrganizadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.infingardi.organizadordetarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizadorDeTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infingardi/organizadordetarefas/OrganizadorDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infingardi.organizadordetarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
