c20d1730463e4d383779307e4889686afdb2b463
igor <infingardidev@gmail.com>
2022-09-03 01:27:15 -0300

    feat: adicionando o repository do user

---
+++ src/main/java/com/infingardi/organizadordetarefas/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.infingardi.organizadordetarefas.repository;
+
+import com.infingardi.organizadordetarefas.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+// Ao extender o JpaRepository a maioria das queries são criadas
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByEmail(String email);
+}
