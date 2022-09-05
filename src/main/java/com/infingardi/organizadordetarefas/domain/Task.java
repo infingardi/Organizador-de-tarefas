7e98327e6d673c74680ac7850e80424eb5e36aa2
igor <infingardidev@gmail.com>
2022-09-05 18:47:14 -0300

    feat: adicionando verificadores nas entidades de task e user

--- src/main/java/com/infingardi/organizadordetarefas/domain/Task.java
+++ src/main/java/com/infingardi/organizadordetarefas/domain/Task.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
@@ -21,15 +23,19 @@ public class Task implements Serializable {
     private UUID id;
 
     @Column(nullable = false)
+    @NotEmpty(message = "The task description cannot be empty")
+    @Size(min = 2)
     private String taskDescription;
 
     @Column(nullable = false)
     private Date data = new Date();
 
     @Column(nullable = false)
+    @NotEmpty(message = "The status cannot be empty")
     private Boolean status;
 
     @Column(nullable = false)
+    @NotEmpty(message = "The userId cannot be empty")
     private String userId;
 
 }
