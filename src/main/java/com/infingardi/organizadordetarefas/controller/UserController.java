6d360748519d5a22d1fd78ed856b846a20589ce6
igor <infingardidev@gmail.com>
2022-09-05 18:49:37 -0300

    feat: adicionando try e catch no controller em user e task

--- src/main/java/com/infingardi/organizadordetarefas/controller/UserController.java
+++ src/main/java/com/infingardi/organizadordetarefas/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.infingardi.organizadordetarefas.controller;
 
 import com.infingardi.organizadordetarefas.domain.User;
+import com.infingardi.organizadordetarefas.dto.TokenDto;
 import com.infingardi.organizadordetarefas.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -15,13 +18,21 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/create")
-    public ResponseEntity<User> create(@RequestBody User user){
-        return ResponseEntity.status(201).body(userService.create(user));
+    public ResponseEntity<User> create(@RequestBody @Valid User user){
+        try {
+            return ResponseEntity.status(201).body(userService.create(user));
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody User user){
-        // Retorna um token
-        return ResponseEntity.status(200).body(userService.login(user));
+    public ResponseEntity<TokenDto> login(@RequestBody @Valid User user){
+        try {
+            // Retorna um token
+            return ResponseEntity.status(200).body(userService.login(user));
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
     }
 }
